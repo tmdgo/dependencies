46f81b62da9280110c06ad22939745dba972c61f
Thiago Mota <thiago@thiagomota.dev>
2021-09-15 14:15:57 +0000

    Initial code commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tmdgo/dependencies
+
+go 1.16
+
+require github.com/tmdgo/reflection v1.0.0
---
+++ manager.go
@@ -0,0 +1,37 @@
+package dependencies
+
+import (
+	"reflect"
+
+	"github.com/tmdgo/reflection/functions"
+	"github.com/tmdgo/reflection/methods"
+)
+
+type Manager struct {
+	dependencies map[reflect.Type]reflect.Value
+}
+
+func (manager *Manager) Init() {
+	manager.dependencies = make(map[reflect.Type]reflect.Value)
+}
+
+func (manager *Manager) InitWithOtherManager(otherManager *Manager) {
+	manager.dependencies = otherManager.dependencies
+}
+
+func (manager *Manager) Add(dependency interface{}) {
+	manager.CallMethodByName(dependency, "Init")
+	manager.dependencies[reflect.TypeOf(dependency)] = reflect.ValueOf(dependency)
+}
+
+func (manager Manager) Get(reflectType reflect.Type) interface{} {
+	return manager.dependencies[reflectType]
+}
+
+func (manager Manager) CallFunc(function interface{}) []reflect.Value {
+	return functions.CallFunc(function, manager.dependencies)
+}
+
+func (manager Manager) CallMethodByName(model interface{}, name string) []reflect.Value {
+	return methods.CallMethodByName(model, name, manager.dependencies)
+}
